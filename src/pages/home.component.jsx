0b2b9027a68cc1adb259f9a2f03344e1140e78b2
Kevin-kirago <tkevinkiragu@gmail.com>
2023-06-15 12:19:06 +0300

    Updated homepage reel video

--- src/pages/home.component.jsx
+++ src/pages/home.component.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import homepageVideo from "../assets/Reel.mp4";
+import homepageVideo from "../assets/Reel-blac.mp4";
 
 const HomePage = () => {
-	return (
-		<div className="home">
-			<video className="video__container" autoPlay loop muted>
-				<source src={homepageVideo} type="video/mp4" />
-			</video>
-		</div>
-	);
+    return (
+        <div className="home">
+            <video className="video__container" autoPlay loop muted>
+                <source src={homepageVideo} type="video/mp4" />
+            </video>
+        </div>
+    );
 };
 
 export default HomePage;
